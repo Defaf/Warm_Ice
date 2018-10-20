a6c7372049ba5ee2d8a2094d5d8878a86bc0a8e0
Dhaffaf <defaf_kamfar@hotmail.com>
2018-10-20 12:33:27 +0300

    warm_ice_v1

---
+++ Assets/Scripts/Particles.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour {
+
+
+	public ParticleSystem snowPS; 
+	public int emitVol; 
+	public float timeVar = 0 ; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeVar += Time.deltaTime; 
+		if(timeVar > .5 ) {//"JSON var should be float "
+			timeVar = 0 ;
+			emitVol +=5 ; //"JSON var"
+		}
+
+		snowPS.Emit(emitVol);
+
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	public CharacterController2D control; 
+	float horizontalMove = 0f; 
+	public float runSpeed = 40f; 
+	bool jump = false; 
+	bool slide = false ; 
+
+	public Animator anim; 
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontalMove = Input.GetAxis ("Horizontal") * runSpeed ;
+		anim.SetFloat ("speed", Mathf.Abs(horizontalMove));
+		if(Input.GetButtonDown("Jump")){// if pree Space bar 
+			jump = true; 
+			anim.SetBool ("isJump",true);
+		}
+		if(Input.GetButtonDown("Slide")){
+			slide = true;
+		}else if(Input.GetButtonDown("Slide")){
+			slide = false ; 
+		}
+	}
+
+	public void OnLoading(){
+		anim.SetBool ("isJump",false);
+	}
+
+	public void OnCrouch(bool slide){
+		anim.SetBool ("slide", slide); 
+	}
+
+	void FixedUpdate(){
+		control.Move (horizontalMove * Time.fixedDeltaTime , slide, jump);
+		jump = false; 
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.CompareTag("coin")){
+			iceScore.points += 1; 
+			Destroy (col.gameObject);
+		}
+		else if(col.CompareTag("item")){
+			itemScore.items += 10; 
+			Destroy (col.gameObject);
+		}
+	}
+		
+}
